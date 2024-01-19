2b63c3f86f2c9da0666ae889b67f2bf7cad62b2d
Noah Söng <noah.song@elev.ga.ntig.se>
2024-01-19 23:45:38 +0100

    fixat så man kan döpa spelarna+ påbörjat loopen

--- src/App.java
+++ src/App.java
@@ -2,20 +2,62 @@ import java.util.Scanner;
 import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner t=new Scanner(System.in);
+
+        //Slumpar tal
         Random tärning= new Random();
         int SpelareSlumpStart = tärning.nextInt(10)+1;
         int DatorSlumpStart = tärning.nextInt(10)+1;
+
+        // Meny och Vem börjar
+        Scanner t=new Scanner(System.in);
         System.out.println("Välkommen till fightspel. Välj val:\n 1. Starta\n 2. Avsluta");
         String val=t.nextLine();
         if (val.equalsIgnoreCase("Starta"))
         {
-        System.out.println("Nu slumpas vem som ska börja. Den som får högst börjar!");
-        System.out.println("Du fick: "+SpelareSlumpStart);
-        System.out.println("Datorn fick: "+DatorSlumpStart);
+            System.out.println("Döp din spelare:");
+            String SpelarNamn=t.nextLine();
+            System.out.println("Döp din motståndare:");
+            String DatorNamn=t.nextLine();
 
-            
 
+
+            System.out.println("Nu slumpas vem som ska börja. Den som får högst börjar!");
+            Thread.sleep(2000);
+            System.out.println(SpelarNamn+" fick: "+SpelareSlumpStart);
+            Thread.sleep(2000);
+            System.out.println(DatorNamn+" fick: "+DatorSlumpStart);
+
+            String VemsTur;
+
+            if (SpelareSlumpStart>DatorSlumpStart)
+            {
+            Thread.sleep(2000);
+            System.out.println(SpelarNamn+" fick högst! " +SpelarNamn+ " börjar");
+            VemsTur="Spelare";
+            }
+
+            else{
+            Thread.sleep(2000);
+            System.out.println(DatorNamn+" fick högst! "+DatorNamn+ " börjar");
+            VemsTur="Dator";
+            }
+        }
+
+        else if (val.equalsIgnoreCase("Avsluta"))
+        {
+            System.out.println("Avslutar spelet....");    
+        }
+
+        else{
+            System.out.println("Fel inmatning! Vänligen välj Starta/Avsluta");
+            }
+
+        int SpelareHP=10;
+        int DatorHP=10;
+
+        while (SpelareHP > 0 && DatorHP > 0)
+        {
+        
         }
     }
 }
